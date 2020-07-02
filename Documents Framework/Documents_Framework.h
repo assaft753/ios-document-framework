8d58255c174bbc040f464470bceeee0ba6ee7e45
Assaf Tayouri <assaft753@gmail.com>
2020-07-02 16:07:06 +0300

    Initial Commit

---
+++ Documents Framework/Documents_Framework.h
@@ -0,0 +1,19 @@
+//
+//  Documents_Framework.h
+//  Documents Framework
+//
+//  Created by Assaf Tayouri on 02/07/2020.
+//  Copyright © 2020 Assaf Tayouri. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Documents_Framework.
+FOUNDATION_EXPORT double Documents_FrameworkVersionNumber;
+
+//! Project version string for Documents_Framework.
+FOUNDATION_EXPORT const unsigned char Documents_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Documents_Framework/PublicHeader.h>
+
+
